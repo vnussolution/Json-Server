abbe85ee1a583b104b49c69041c18104d9a2d4eb
frank <vnussolution@gmail.com>
2017-08-20 15:54:41 -0700

    add moment

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var http = require('http');
+var moment = require('moment-timezone');
+moment().tz("America/Los_Angeles").format();
+
+var timestamp = 1403454068850,
+    date = new Date(timestamp);
 
 var server = http.createServer(function (request, response) {
 
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello World!");
+    response.end("Hello World! " + moment.tz(date, "America/Los_Angeles").format());
 
 });
 
