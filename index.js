0e69be9360a69e6c9f43d20446fc49483b93935e
frank <vnussolution@gmail.com>
2017-08-20 15:56:54 -0700

    update

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-var http = require('http');
-var moment = require('moment-timezone');
-moment().tz("America/Los_Angeles").format();
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+//const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults();
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
-var timestamp = 1403454068850,
-    date = new Date(timestamp);
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
 
-var server = http.createServer(function (request, response) {
+function isAuthorized() {
+    console.log('is authorized');
+    return true;
+}
 
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello World! " + moment.tz(date, "America/Los_Angeles").format());
-
+server.use((req, res, next) => {
+    console.log(req);
+    if (isAuthorized(req)) { // add your authorization logic here
+        next() // continue to JSON Server router
+    } else {
+        res.sendStatus(401)
+    }
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+server.get('/', (req, res) => {
+    res.render('Xin chao cac ban :)');
+});
 
-console.log("Server running at http://localhost:%d", port);
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+    res.jsonp(req.query)
+})
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+    if (req.method === 'POST') {
+        req.body.createdAt = Date.now()
+    }
+    // Continue to JSON Server router
+    next()
+})
+
+// Use default router
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
+
